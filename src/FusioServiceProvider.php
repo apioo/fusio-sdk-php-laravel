44abf07ba2fb8ba3c81d5a1f33182c9840788a80
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-08-06 23:06:46 +0200

    update provider

--- src/FusioServiceProvider.php
+++ src/FusioServiceProvider.php
@@ -8,7 +8,6 @@ use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use Sdkgen\Client\TokenStore\FileTokenStore;
 use Sdkgen\Client\TokenStore\MemoryTokenStore;
-use Sdkgen\Client\TokenStoreInterface;
 
 /**
  * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
@@ -16,61 +15,42 @@ use Sdkgen\Client\TokenStoreInterface;
  */
 class FusioServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    /**
-     * @return void
-     */
-    public function boot()
+    public array $singletons = [
+        MemoryTokenStore::class => MemoryTokenStore::class,
+        SessionTokenStore::class => SessionTokenStore::class,
+    ];
+
+    public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/../config/fusio.php' => config_path('fusio.php'),
-            'fusio'
+            __DIR__ . '/../config/fusio.php' => config_path('fusio.php')
         ]);
     }
 
-    /**
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/fusio.php', 'fusio');
+        $this->app->singleton(FileTokenStore::class, function (App $app) {
+            return new FileTokenStore(storage_path('framework/cache'));
+        });
 
         $this->app->singleton(Client::class, function (App $app) {
-            /** @var TokenStoreInterface $store */
-            $tokenStore = $app->make(FileTokenStore::class);
-
             return new Client(
                 config('fusio.base_uri'),
                 config('fusio.app_key'),
                 config('fusio.app_secret'),
                 config('fusio.scopes'),
-                $tokenStore
+                $app->make(FileTokenStore::class)
             );
         });
-
-        $this->app->singleton(FileTokenStore::class, function () {
-            return new FileTokenStore();
-        });
-
-        $this->app->singleton(MemoryTokenStore::class, function () {
-            return new MemoryTokenStore();
-        });
-
-        $this->app->singleton(SessionTokenStore::class, function () {
-            return new SessionTokenStore();
-        });
-
-        // by default we use the memory token store, if you want to persist the access token between requests you need
-        // to use a different implementation
-        $this->app->bind(TokenStoreInterface::class, SessionTokenStore::class);
     }
 
-    /**
-     * @return array
-     */
-    public function provides()
+    public function provides(): array
     {
         return [
             Client::class,
+            FileTokenStore::class,
+            MemoryTokenStore::class,
+            SessionTokenStore::class,
         ];
     }
 }
